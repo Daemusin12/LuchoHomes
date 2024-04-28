9afb5901b442447b7f914f66387498dc3efe5ba2
MSI\Leo <engrleonelfelezario812@gmail.com>
2024-04-28 23:27:49 +0800

    Generate housing-location component

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.scss'
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
